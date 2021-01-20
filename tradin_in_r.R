3d50790b225ff67253dca88bc687f5124c75ba5c
Miguel Moreno <contact@miguelmath.com>
2021-01-20 19:24:29 +0200

    Datasets obtained, plot and cleaned

--- tradin_in_r.R
+++ tradin_in_r.R
@@ -1,14 +1,85 @@
 ?PerformanceAnalytics
 ?quantmod
-library (quantmod)
-library (PerformanceAnalytics)
+library(quantmod)
+library(PerformanceAnalytics)
+library(dplyr)
+library(caret)
+library(tidyverse)
 
 
-
+#choose the date to start collecting the data
 date <- "2020-1-1"
-BCClose <- getSymbols.yahoo("BTC-USD", from = date, auto.assign = F)[,6]
-BCClose
 
-BCRets <- dailyReturn(BCClose, type = "log")
+#collect the data from yahoo
+LTCUSD <- getSymbols.yahoo("LTC-USD", from = date, auto.assign = F)[,] #Litecoin
+ETHUSD <- getSymbols.yahoo("ETH-USD", from = date, auto.assign = F)[,] #Ethereum
+XRPUSD <- getSymbols.yahoo("XRP-USD", from = date, auto.assign = F)[,] #Ripple
+BTCUSD <- getSymbols.yahoo("BTC-USD", from = date, auto.assign = F)[,] #Bitcoin
 
-chartSeries(BCRets)
+#extract highest value of the day
+LTCHigh <- LTCUSD[,2]
+ETHHigh <- ETHUSD[,2]
+XRPHigh <- XRPUSD[,2]
+BTCHigh <- BTCUSD[,2]
+
+#graphic the data
+chartSeries(LTCHigh)
+chartSeries(ETHHigh)
+chartSeries(XRPHigh)
+chartSeries(BTCHigh)
+
+#extract closed value of the day
+LTCClose <- LTCUSD[,4]
+ETHClose <- ETHUSD[,4]
+XRPClose <- XRPUSD[,4]
+BTCClose <- BTCUSD[,4]
+
+#graphic the data
+chartSeries(LTCClose)
+chartSeries(ETHClose)
+chartSeries(XRPClose)
+chartSeries(BTCClose)
+
+#extract the adjusted value of the day
+LTCAdjust <- LTCUSD[,6]
+ETHAdjust <- ETHUSD[,6]
+XRPAdjust <- XRPUSD[,6]
+BTCAdjust <- BTCUSD[,6]
+
+#extract the returns
+LTCRets <- na.omit(dailyReturn(LTCClose, type = "log"))
+ETHRets <- na.omit(dailyReturn(ETHClose, type = "log"))
+XRPRets <- na.omit(dailyReturn(XRPClose, type = "log"))
+BTCRets <- na.omit(dailyReturn(BTCClose, type = "log"))
+
+#graph the returns
+chartSeries(LTCRets)
+chartSeries(ETHRets)
+chartSeries(XRPRets)
+chartSeries(BTCRets)
+
+#Prepare the data frames
+dfLTC <- data.frame(LTCUSD)
+LTCwith_ticket <- mutate(dfLTC, Company_Ticket = "LTC-USD", Date = row.names(dfLTC))
+row.names(LTCwith_ticket) <- 1:nrow(LTCUSD)
+colnames(LTCwith_ticket) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted", "Company_Ticket", "Date")
+
+dfETH <- data.frame(ETHUSD)
+ETHwith_ticket <- mutate(dfETH, Company_Ticket="ETH-USD", Date = row.names(dfETH))
+row.names(ETHwith_ticket) <- 1:nrow(ETHUSD)
+colnames(ETHwith_ticket) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted", "Company_Ticket", "Date")
+
+dfXRP <- data.frame(XRPUSD)
+XRPwith_ticket <- mutate(dfXRP, Company_Ticket="XRP-USD", Date = row.names(dfXRP))
+row.names(XRPwith_ticket) <- 1:nrow(XRPUSD)
+colnames(XRPwith_ticket) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted", "Company_Ticket", "Date")
+
+dfBTC <- data.frame(BTCUSD)
+BTCwith_ticket <- mutate(dfBTC, Company_Ticket="BTC-USD", Date = row.names(dfBTC))
+row.names(BTCwith_ticket) <- 1:nrow(BTCUSD)
+colnames(BTCwith_ticket) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted", "Company_Ticket", "Date")
+
+
+#bigdat <- rbind(dfLTC, dfETH)
+
+
